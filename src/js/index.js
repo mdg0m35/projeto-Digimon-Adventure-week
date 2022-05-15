882a3835e58a44f18142d228a217d97c5a460f66
Marcos David <57731043+mdg0m35@users.noreply.github.com>
2022-05-15 15:20:11 -0300

    2 commit

---
+++ src/js/index.js
@@ -0,0 +1,66 @@
+/* 
+ OBJETIVO 1 - quando passar o mouse em cima do personagem temos que:
+
+  - colocar a classe selecionado no personagem que passamos o mouse em cima para adicionar a animação nele 
+
+ - retirar a classe selecionado do personagem que está selecionado
+
+
+ OBJETIVO 2 - quando passar o mouse em cima do personagem, trocar a imagem e nome do personagem grande
+
+    - alterar a imagem do jogador 1
+    - alterar o nome do jogador 1
+*/
+
+const personagens = document.querySelectorAll(".personagem");
+
+personagens.forEach((personagem) => {
+
+  personagem.addEventListener("mouseenter", () => {
+
+    const idSelecionado = personagem.attributes.id.value;
+
+    if(idSelecionado === 'ultron' ) return;
+
+    if(idSelecionado === 'logo'){
+      const audio = document.getElementById('audioLogo').play()
+
+    }
+    /* 
+        OBJETIVO 1 - quando passar o mouse em cima do personagem temos que:
+
+        - colocar a classe selecionado no personagem que passamos o mouse em cima para adicionar a animação nele 
+
+        - retirar a classe selecionado do personagem que está selecionado
+    */
+    const personagemSelecionado = document.querySelector(".selecionado");
+    personagemSelecionado.classList.remove("selecionado");
+
+    personagem.classList.add("selecionado");
+    const audio = document.getElementById('audio').play()
+
+    /* 
+        OBJETIVO 2 - quando passar o mouse em cima do personagem da listagem, trocar a imagem e nome do personagem grande
+            - alterar a imagem do jogador 1
+            - alterar o nome do jogador 1
+    */
+    
+    const imagemJogador1 = document.getElementById('personagem-jogador-1');
+    imagemJogador1.src = `./src/imagens/${idSelecionado}.png`;
+    
+    
+   
+
+    const imagemJogador1Brasao = document.getElementById('personagem-jogador-1Brasao');
+    imagemJogador1Brasao.src = `./src/imagens/${idSelecionado}-.png`;
+    
+    
+  
+
+
+    const nomeJogador1 = document.getElementById('nome-jogador-1');
+    const nomeSelecionado = personagem.getAttribute('data-name');
+    
+    nomeJogador1.innerHTML = nomeSelecionado;
+  });
+});
